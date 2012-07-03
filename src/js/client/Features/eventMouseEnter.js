4e535b3fc702e6d8884e62fa4232230368036798
mytharcher <mytharcher@gmail.com>
2012-07-03 15:05:02 +0800

    Change compatible event 'mouseenter'/'mouseleave' form browser based to feature based.

---
+++ src/js/client/Features/eventMouseEnter.js
@@ -0,0 +1,17 @@
+/*
+ * jslib JavaScript Library
+ * 
+ * create:
+ * @2012-07-02 by mytharcher
+ * 
+ * update:
+ */
+
+///import js.client.Features;
+
+/**
+ * 是否支持mouseenter事件
+ * @property js.client.Features.eventMouseEnter
+ * @type {Boolean}
+ */
+js.client.Features.eventMouseEnter = 'onmouseenter' in document.body;
---
+++ src/js/client/Features/eventMouseLeave.js
@@ -0,0 +1,17 @@
+/*
+ * jslib JavaScript Library
+ * 
+ * create:
+ * @2012-07-02 by mytharcher
+ * 
+ * update:
+ */
+
+///import js.client.Features;
+
+/**
+ * 是否支持onmouseleave事件
+ * @property js.client.Features.eventMouseLeave
+ * @type {Boolean}
+ */
+js.client.Features.eventMouseLeave = 'onmouseleave' in document.body;
