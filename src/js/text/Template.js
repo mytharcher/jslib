5d2d8b870cbf4c0702e2117ce5c0ac6f86d46d28
mytharcher <mytharcher@gmail.com>
2014-04-12 21:40:33 +0800

    Fix Template typo error.

--- src/js/text/Template.js
+++ src/js/text/Template.js
@@ -117,7 +117,7 @@ js.text.Template = js.text.Template || {
 	 */
 	delimiter: function (left, right) {
 		var Template = js.text.Template,
-			escapeReg = js.text.Escaper.escapeRegExp;
+			escapeRegExp = js.text.Escaper.escapeRegExp;
 		Template._re = new RegExp(escapeRegExp(left || Template.LEFT_DELIMITER) + '(\\w+)' + escapeRegExp(right || Template.RIGHT_DELIMITER), 'ig');
 	}
 };
